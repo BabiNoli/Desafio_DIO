e2e5c8c5e14b4538a2f5b237c3d32194041995cc
Barbara Noli <babinolidesign@gmail.com>
2025-03-01 06:12:33 +0100

    first commit

---
+++ logica_programacao.js
@@ -0,0 +1,41 @@
+do{    
+    try {
+        let xp = parseInt(prompt("Digite a quantidade de XP: "));
+        console.log("Você possui " + xp + " XP");  
+        if (isNaN(xp)) {
+            throw new Error("Valor inválido. Não é um número.");
+        }
+        if (xp < 0) {
+            throw new Error("Valor inválido. Não pode ser negativo.");
+        }
+    } catch (error) {
+        console.log("Erro: " + error.message);   
+    }
+} while (isNaN(xp) || xp < 0);
+
+switch (true) {
+  case xp >= 0 && xp <= 1000:
+    console.log("Você está no nível Ferro");
+    break;
+  case xp >= 1001 && xp <= 2000:
+    console.log("Você está no nível Bronze");
+    break;
+  case xp >= 2001 && xp <= 5000:
+    console.log("Você está no nível Prata");
+    break;
+  case xp >= 5001 && xp <= 7000:
+    console.log("Você está no nível Ouro");
+    break;
+  case xp >= 7001 && xp <= 8000:
+    console.log("Você está no nível Platina");
+    break;
+  case xp >= 8001 && xp <= 9000:
+    console.log("Você está no nível Ascendente");
+    break;
+  case xp >= 9001 && xp <= 10000:
+    console.log("Você está no nível Imortal");
+    break;
+  default:
+    console.log("Você está no nível Radiante");
+    break;
+}
